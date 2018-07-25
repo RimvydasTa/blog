8954ccb76182e5fc081e4c8cbb3d4f847abf0639
Rimvydas Tamošiūnas <rimvydas.tamosiunas@unishop.lt>
2018-07-25 15:24:01 +0300

    initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Blog Template for Bootstrap</title>
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<header>
+
+        @include('layouts.nav')
+</header>
+    <div class="container">
+        @yield('content')
+    </div>
+@include('layouts.footer')
+
+</body>
+</html>
