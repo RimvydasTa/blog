e479525e4f38266489666a68ea8ffc8c38534af8
rimvydas <tamosiunas.rimvydas@gmail.com>
2018-08-05 22:18:33 +0300

    auth

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,11 +12,27 @@
         @foreach($post->comments as $comment)
             <li class="list-group-item">
                 <strong>
-                    {{$comment->created_at->diffFormHumans()}}:
+                    {{$comment->created_at->diffFormHumans()}}
                 </strong>
                 {{$comment->body}}
             </li>
             @endforeach
         </ul>
+    </div>
+    <div class="card">
+        <div class="card-block">
+            <form class="" method="post" action="/posts/{{$post->id}}/comments">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <textarea class="form-control" name="body" placeholder="Your comment here" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        Add comment
+                    </button>
+                </div>
+            </form>
+            @include('errors.error')
+        </div>
     </div>
     @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,12 @@
     Route::get('/posts/{post}', 'PostsController@show');
     Route::get('/posts/create', 'PostsController@create');
     Route::post('/posts', 'PostsController@store');
+    Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
